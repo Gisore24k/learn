ee62f77e8b4356479cfe055ce4c975d1aef2b9c1
abimon <eddyabby59@gmail.com>
2025-04-20 10:54:02 +0300

    Changed the app.js file paths to cover backend prefix

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -12,27 +12,27 @@
 
 
     <!-- App favicon -->
-    <link rel="shortcut icon" href=" {{asset('backend/assets/images/favicon.ico')}}">
+    <link rel="shortcut icon" href=" {{asset('/backend/assets/images/favicon.ico')}}">
 
     <!-- jquery.vectormap css -->
-    <link href=" {{asset('backend/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css')}}"
+    <link href=" {{asset('/backend/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css')}}"
         rel="stylesheet" type="text/css" />
 
     <!-- DataTables -->
-    <link href=" {{asset('backend/assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet"
+    <link href=" {{asset('/backend/assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet"
         type="text/css" />
 
     <!-- Responsive datatable examples -->
-    <link href=" {{asset('backend/assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css')}}"
+    <link href=" {{asset('/backend/assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css')}}"
         rel="stylesheet" type="text/css" />
 
     <!-- Bootstrap Css -->
-    <link href=" {{asset('backend/assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet"
+    <link href=" {{asset('/backend/assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet"
         type="text/css" />
     <!-- Icons Css -->
-    <link href=" {{asset('backend/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href=" {{asset('/backend/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
     <!-- App Css-->
-    <link href=" {{asset('backend/assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css" />
+    <link href=" {{asset('/backend/assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css" />
 
 </head>
 
@@ -799,35 +799,35 @@
     <div class="rightbar-overlay"></div>
 
     <!-- JAVASCRIPT -->
-    <script src=" {{asset('backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script src=" {{asset('/backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src=" {{asset('backend/assets/libs/metismenu/metisMenu.min.js')}}"></script>
-    <script src=" {{asset('backend/assets/libs/simplebar/simplebar.min.js')}}"></script>
-    <script src=" {{asset('backend/assets/libs/node-waves/waves.min.js')}}"></script>
+    <script src=" {{asset('/backend/assets/libs/metismenu/metisMenu.min.js')}}"></script>
+    <script src=" {{asset('/backend/assets/libs/simplebar/simplebar.min.js')}}"></script>
+    <script src=" {{asset('/backend/assets/libs/node-waves/waves.min.js')}}"></script>
 
 
     <!-- apexcharts -->
-    <script src=" {{asset('backend/assets/libs/apexcharts/apexcharts.min.js')}}"></script>
+    <script src=" {{asset('/backend/assets/libs/apexcharts/apexcharts.min.js')}}"></script>
 
     <!-- jquery.vectormap map -->
     <script
-        src=" {{asset('backend/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js')}}"></script>
+        src=" {{asset('/backend/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js')}}"></script>
     <script
-        src=" {{asset('backend/assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-us-merc-en.js')}}"></script>
+        src=" {{asset('/backend/assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-us-merc-en.js')}}"></script>
 
     <!-- Required datatable js -->
-    <script src=" {{asset('backend/assets/libs/datatables.net/js/jquery.dataTables.min.js')}}"></script>
-    <script src=" {{asset('backend/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+    <script src=" {{asset('/backend/assets/libs/datatables.net/js/jquery.dataTables.min.js')}}"></script>
+    <script src=" {{asset('/backend/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
 
     <!-- Responsive examples -->
-    <script src=" {{asset('backend/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js')}}"></script>
+    <script src=" {{asset('/backend/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js')}}"></script>
     <script
-        src=" {{asset('backend/assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js')}}"></script>
+        src=" {{asset('/backend/assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js')}}"></script>
 
-    <script src=" {{asset('backend/assets/js/pages/dashboard.init.js')}}"></script>
+    <script src=" {{asset('/backend/assets/js/pages/dashboard.init.js')}}"></script>
 
     <!-- App js -->
-    <script src=" {{asset('backend/assets/js/app.js')}}"></script>
+    <script src=" {{asset('/backend/assets/js/app.js')}}"></script>
 </body>
 
 </html>
